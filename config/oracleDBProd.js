a45e63d60d3bbc331ede3959ccb7b6b8101fc2c6
David DA. Argueta <dargueta@ipg.com.gt>
2022-02-28 11:13:27 -0600

    jornada finished

--- config/oracleDBProd.js
+++ config/oracleDBProd.js
@@ -2,7 +2,8 @@ module.exports = {
     user: "bi",
     password: process.env.NODE_ORACLEDB_PASSWORD_PROD,
     // password: 'manager1',
-    connectString:"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host="+'198.200.1.208'+")(Port="+'1521'+"))(CONNECT_DATA=(SID="+'dbprodpi'+")))"
+    connectString:"(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host="+'198.200.1.208'+")(Port="+'1521'+"))(CONNECT_DATA=(SID="+'dbprodpi'+")))",
+    poolAlias : 'prod'
 };
 
 // DBPRODPI = 
--- sockets/controller.js
+++ sockets/controller.js
@@ -11,6 +11,7 @@ const moment = require('moment');
 let connectionsArray = [];
 let POLLING_INTERVAL = 60000;
 // let POLLING_INTERVAL = 5000;
+
 let pollingTimer;
 
 let conexion;
@@ -27,15 +28,15 @@ const conectaOracle = async(connection) => {
     if (libPath && fs.existsSync(libPath)) {
       oracledb.initOracleClient({ libDir: libPath });
       try {
-          conexion = await oracledb.getConnection(dbOracle);
-          conexion_prod = await oracledb.getConnection(dbOracleProd);
-          console.log('Oracle conectado correctamente')
+            await oracledb.createPool(dbOracleProd);
+            await oracledb.createPool(dbOracle);
+            console.log('Oracle conectado correctamente')
       } catch (err) {
           console.error(err.message);
       }
     }
 }
-
+conectaOracle();
 const getAsistencia = async()=>{
     try{
         const asistencia = await Asistencia.findAll({
@@ -55,18 +56,27 @@ const getAsistencia = async()=>{
                     where: {estado : true, repit: true},
                     order : [['id', 'ASC']]
                   });
+                  
+                  conexion = await oracledb.getConnection('qa');
                   const usuario = await conexion.execute("select user from dual",{}, { outFormat: oracledb.OBJECT }); 
                   const fecha_registro = await conexion.execute("select sysdate from dual");
+                //   console.log(usuario, fecha_registro)
                   const user = usuario.rows[0].USER;
                   const fecha_db = fecha_registro.rows[0][0];
+
+                  
+                  await conexion.close();
                   let jornada;
                   for(let j =0; j<asistencia1.length; j++){
                     const fecha_hora = asistencia1[j].fecha_aut + ' ' + asistencia1[j].hora_aut;
                     
                     const no_cia  = asistencia1[j].no_tarjeta.substring(0,2);
+                    // const no_cia  = '03';
                     const codpla =  asistencia1[j].no_tarjeta.substring(2,4); 
+                    // const codpla =  '256366598'; 
                     const no_emple = Number(asistencia1[j].no_tarjeta.substring(9));
                     
+                    conexion_prod = await oracledb.getConnection('prod');
                     const empleado =  await conexion_prod.execute(`SELECT a.nombre, a.no_cia, b.jornada, a.no_emple, a.codpla , a.no_hora,
                         b.entra_l lunes, b.entra_m martes, b.entra_k miercoles, b.entra_j jueves, b.entra_v viernes, b.entra_s sabado, b.entra_d domingo,
                         b.sale_l lunes_sale, b.sale_m martes_sale, b.sale_k miercoles_sale, b.sale_j jueves_sale, b.sale_v viernes_sale, b.sale_s sabado_sale, b.sale_d domingo_sale
@@ -76,7 +86,11 @@ const getAsistencia = async()=>{
                             and a.NO_HORA	= b.NO_HORA
                             where a.NO_EMPLE = '${no_emple}' and a.no_cia = '${no_cia}' and a.codpla = ${codpla}
                     `);
-                    if(empleado.rows[0][2] === '1'){
+                    
+                    await conexion_prod.close();
+                    // console.log(empleado);
+                    if(empleado.rows.length>0){
+                        if(empleado.rows[0][2] === '1'){
                             const hora_string = asistencia1[j].hora_aut.toString();
                             const hora_number = Number(hora_string.substring(0,2));
                             const hora_entrada = hora_number * 3600;
@@ -92,23 +106,29 @@ const getAsistencia = async()=>{
                             ruta = EntradaSalida(hora_entrada,entrada_laboral, salida_laboral);
                             cambio_jornada =  (ruta == 'SALIDA') ? true :false;
 
+                        }else{
+                            cambio_jornada = false;
+                        } 
                     }else{
                         cambio_jornada = false;
-                    }   
+                    }
+                      
                     jornada = new Date(fecha_hora);
                     jornada.setDate(jornada.getDate() -1);
                     
                     
                     const jornada_actual = (cambio_jornada == true) ?customFormatter(jornada):customFormatter(asistencia1[j].fecha_aut);
 
-                    // console.log(jornada_actual,  jornada, asistencia1[j].fecha_aut);
-
+                    // console.log(jornada_actual,  jornada);
+                    conexion = await oracledb.getConnection('qa');
                     await conexion.execute("INSERT INTO "+'planilla.arplmarcas'+" VALUES "+
                     "(:NUMERO, :NO_EMPLE, TO_DATE(:FECHA_HORA_AUT,'RRRR/MM/DD hh24:mi:ss'), TO_DATE(:FECHA_AUT,'RRRR/MM/DD'), :HORA_AUT, :DIRECCION, :ENTRADA, :SERIE, :NOMBRE, :NO_TARJETA, :INGRESO, :SALIDA, :USUARIO, :FECHA_CREACION, :ESTADO, TO_DATE(:JORNADA,'RRRR/MM/DD'))",
                     [asistencia1[j].id,  asistencia1[j].no_emple,fecha_hora, 
                     asistencia1[j].fecha_aut,asistencia1[j].hora_aut, asistencia1[j].direccion, asistencia1[j].entrada, asistencia1[j].serie
                     ,asistencia1[j].nombre, asistencia1[j].no_tarjeta, asistencia1[j].ingreso, asistencia1[j].salida, user, fecha_db, '1',jornada_actual ],{ autoCommit: true }); 
                     await Asistencia.update({estado: false},{where : {id : asistencia1[j].id}});
+                    await conexion.close();
+
                 }
             }
           
@@ -157,7 +177,7 @@ const Hora= (dia_actual, E_S)=>{
 }
 
 // conectaOracleProd();
-conectaOracle();
+
 module.exports = {
     getAsistencia
 }
